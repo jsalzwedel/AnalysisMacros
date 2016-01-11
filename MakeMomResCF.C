a590d287a6c2398c13fcbf695882dce158d5d713
Jai Salzwedel <jai.salzwedel@cern.ch>
2016-01-11 11:25:23 -0500

    Added function to make unsliced momentum resolution CFs

--- MakeMomResCF.C
+++ MakeMomResCF.C
@@ -14,3 +14,23 @@ void MakeMomResCF()
   cf->DrawCopy();
 
 }
+
+void MakeUnslicedMomResCF()
+{
+  TFile f("Projections.root");
+  TDirectory *dirData = f.GetDirectory("ResolutionLL");
+  // TDirectory *dirNum = dirData->GetDirectory("kstarVsPtNum");
+  // TDirectory *dirDen = dirData->GetDirectory("kstarVsPtDen");
+
+  TH1D *num = (TH1D*) dirData->Get("fSignalMomResTruthVsPtLL_px");
+  TH1D *den = (TH1D*) dirData->Get("fBkgMomResTruthVsPtLL_px");
+  
+  TH1D *cf = (TH1D*) num->Clone("CF");
+  Double_t numScale = num->Integral(num->FindBin(1.), num->FindBin(1.4));
+  Double_t denScale = den->Integral(den->FindBin(1.), den->FindBin(1.4));
+  cf->Scale(1./numScale);
+  den->Scale(1./denScale);
+  cf->Divide(den);
+  cf->DrawCopy();
+
+}
