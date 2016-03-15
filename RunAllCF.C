bcd2e8aba253daa32b512d6c6b5a4cdd6c33b51e
Jai Salzwedel <jai.salzwedel@cern.ch>
2016-03-15 16:46:54 -0400

    Added full path to macros to make running macro easier Updated RunAllCF to pass params to macros

--- RunAllCF.C
+++ RunAllCF.C
@@ -1,7 +1,7 @@
-#include "MakeOutputProjections.C"
-#include "MakeCFs.C"
-#include "MakeCombinedCFs.C"
-#include "RebinNumDen.C"
+#include "/home/jai/Analysis/lambda/AliAnalysisLambda/Results/macros/MakeOutputProjections.C"
+#include "/home/jai/Analysis/lambda/AliAnalysisLambda/Results/macros/MakeCFs.C"
+#include "/home/jai/Analysis/lambda/AliAnalysisLambda/Results/macros/MakeCombinedCFs.C"
+#include "/home/jai/Analysis/lambda/AliAnalysisLambda/Results/macros/RebinNumDen.C"
 
 
 
@@ -11,12 +11,15 @@ void RunAllCF(Bool_t isDataCompact, Bool_t isTrainResult)
   MakeCFProjections(isDataCompact, isTrainResult);
 
   // Make CFs from the (unmerged) data
-  MakeCFs(isDataCompact, isTrainResult);
+  Double_t lowNorm = 0.5;
+  Double_t highNorm = 0.7;
+  Int_t rebinNumber = 4;
+  MakeCFs(isDataCompact, isTrainResult, lowNorm, highNorm, rebinNumber);
 
   // Combine fields, centralities and LLAA for each cut
   MakeCombinedCFs(isDataCompact, isTrainResult);
 
   // Rebin the numerator and denominator distributions
   // for use with log fitting
-  RebinNumDen(isDataCompact, isTrainResult);
+  RebinNumDen(isDataCompact, isTrainResult, rebinNumber);
 }
