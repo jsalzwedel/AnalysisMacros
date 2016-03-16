340a9d49c1cff4dc6a56aacc768a158c4da3ef24
Jai Salzwedel <jai.salzwedel@cern.ch>
2016-03-16 11:28:00 -0400

    Added ~ empty file for new macro to analyze systematics

---
+++ AnalyzeSystematics.C
@@ -0,0 +1,10 @@
+// Description...
+
+#include "/home/jai/Analysis/lambda/AliAnalysisLambda/Results/macros/RogerBarlowHelper.C"
+
+TH1D *GetSystematicDifference(TH1D *referenceHist, TH1D *tweakHist)
+{
+  
+
+
+}
