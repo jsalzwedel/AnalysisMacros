c7469ec395a6d45234b51330edf11307400ec7aa
Jai Salzwedel <jai.salzwedel@cern.ch>
2016-03-29 13:53:28 -0400

    Added functions to convert TF1s into vector<Double>. Added function to combine multiple vector<Double> in quadrature Continued to flesh out CombineSystematics()

--- CombineSystematics.C
+++ CombineSystematics.C
@@ -70,13 +70,60 @@ vector< vector<TF1*> > SortTF1s(vector<TF1*> &vecAll)
   return sortedVec;
 }
 
-TF1* AddTF1s(vector<TF1*> &vec) {
+vector<Double_t> ConvertTF1ToVector(TF1* fit) {
+  // Take a fit, convert it into a TH1,
+  // then use that to make a vector of doubles
 
+  // Get the histogram equivalent of the fit.
+  TH1D *hist = fit->GetHistogram();
+  Int_t nBins = hist->GetNbinsX();
+  vector<Double_t> valVec;
+  for (Int_t iBin = 1; iBin < nBins + 1; iBin++) {
+    valVec.push_back(hist->GetBinContent(iBin));
+  }
+  return valVec;
 }
 
-TF1* AddTF1sInQuadrature(vector<TF1*> &vec, Bool_t isPosErrors) {
+void CombinePosNegSeparately(vector <vector <Double_t> > errorVectors,
+			     vector<Double_t> &posVals,
+			     vector<Double_t> &negVals)
+{
+  // Get all the error vectors.  Combine all the postive
+  // values in quadrature, and combine all the negative values
+  // in quadrature
+  assert(posVals.size() == negVals.size());
+
+  // Grab an error vector
+  for (UInt_t iErrVec = 0; iErrVec < errVectors.size(); iErrVec++) {
+    vector<Double_t> &thisVec = errorVectors[iErrVec];
+    assert(thisVec.size() == posVals.size());
+
+    // Loop over each bin in the vector
+    for (UInt_t iBin = 0; iBin < thisVec.size(); iBin++) {
+      Double_t errVal = thisVec[iBin];
+
+      // Decide whether to add to posVec or negVec,
+      // then add in quadrature
+      if(errVal > 0) {
+	posVals[iBin] += pow(errVal,2.);
+      } else {
+	negVals[iBin] += pow(errVal,2.);
+      }
+    }
+  }
+
+  // Sqrt the results for each bin, and set negVals to be negative
+  for (UInt_t iBin = 0; iBin < posVals.size(); iBin++) {
+    posVals[iBin] = sqrt(posVals[iBin]);
+    negVals[iBin] = sqrt(negVals[iBin]);
+    negVals[iBin] *= -1.;
+  } 
+}
+
+TF1* AddConstantTF1sInQuadrature(vector<TF1*> &vec, Bool_t isPosErrors) {
   // Add positive errors in quadrature (or negative if !isPosErrors)
   // This will not work as desired if the TF1s have different ranges.
+  // *** Only works for TF1s in the form f(x) = [0] ***
   Double_t quadPar = 0.;
   for (UInt_t iFit = 0; iFit < vec.size(); iFit++) {
     TF1 *fit = vec[iFit];
@@ -96,5 +143,21 @@ TF1* AddTF1sInQuadrature(vector<TF1*> &vec, Bool_t isPosErrors) {
 void CombineSystematics() {
   vector<TF1*> vecAll = GetAllTF1sFromFile("CFs.root");
   vector< vector<TF1*> > sortedVec = SortTF1s(vecAll);
+
+  // Loop over the different cf systems
+  for (UInt_t iSys = 0; iSys < sortedVec.size(); iSys++) {
+    vector< vector<Double_t> > errVecs;
+
+    // Turn all the fits into vectors of doubles
+    for (UInt_t iFit = 0; iFit < sortedVecs[iSys].size(); iFit++) {
+      errVecs.push_back(ConvertTF1ToVector(sortedVecs[iSys][iFit]));
+    }
+
+    vector<Double_t> posVals;
+    vector<Double_t> negVals;
+    CombinePosNegSeparately(errVecs, posVals, negVals);
+    TGraphAsymmErrors graphAsymm = ConstructAsymmTGraphAtZero(posVals, negVals);
+    
+  }
 }
 
