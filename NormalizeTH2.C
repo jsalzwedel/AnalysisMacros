e0115277cf6492bb8e024f50d7bd8e7b2e1c7fae
Jai Salzwedel <jai.salzwedel@cern.ch>
2016-05-10 10:39:44 -0400

    Fixed bugs in NormalizeTH2 Added test function Added wrapper functions

--- NormalizeTH2.C
+++ NormalizeTH2.C
@@ -6,7 +6,7 @@ TH2 *NormalizeTH2(TH2* hist, TString optionAxis)
     TH2* normHist = (TH2*)hist->Clone(newName);
 
     TAxis *normAxis = NULL;
-    TAXIS *otherAxis = NULL;
+    TAxis *otherAxis = NULL;
     Bool_t isRowNorm = kTRUE;
     if ((optionAxis == "X") || (optionAxis == "x")) {
         normAxis = hist->GetXaxis();
@@ -25,31 +25,108 @@ TH2 *NormalizeTH2(TH2* hist, TString optionAxis)
 
     if (isRowNorm) {
         // Normalizing rows
-        for (Int_t iRow = 1; iRow <= nBins; iRow++) {
+        for (Int_t iRow = 1; iRow <= nBinsNorm; iRow++) {
             Double_t norm = 0.;
             for (Int_t iCol = 1; iCol < otherAxis->GetNbins(); iCol++) {
-                norm += hist->GetBinContent(iRow, iCol);
+                norm += hist->GetBinContent(iCol, iRow);
             }
             if (norm < 0.01) continue; // Avoid divide by zero
             // Now normalize the row
-            for (Int_t iCol = 1; iCol < otherAxis->GetNbins(); iCol++) {
-                Double_t binVal = normHist->GetBinContent(iRow,iCol);
-                normHist->SetBinContent(iRow, iCol, binVal/norm);
+            for (Int_t iCol = 1; iCol <= otherAxis->GetNbins(); iCol++) {
+                Double_t binVal = normHist->GetBinContent(iCol, iRow);
+                normHist->SetBinContent(iCol, iRow, binVal/norm);
             }
         }
     } else {
-        for (Int_t iCol = 1; iCol <= nBins; iCol++) {
+        for (Int_t iCol = 1; iCol <= nBinsNorm; iCol++) {
             Double_t norm = 0.;
             for (Int_t iRow = 1; iRow < otherAxis->GetNbins(); iRow++) {
-                norm += hist->GetBinContent(iRow, iCol);
+                norm += hist->GetBinContent(iCol, iRow);
             }
             if (norm < 0.01) continue; // Avoid divide by zero
             // Now normalize the row
-            for (Int_t iRow = 1; iRow < otherAxis->GetNbins(); iRow++) {
-                Double_t binVal = normHist->GetBinContent(iRow,iCol);
-                normHist->SetBinContent(iRow, iCol, binVal/norm);
+            for (Int_t iRow = 1; iRow <= otherAxis->GetNbins(); iRow++) {
+                Double_t binVal = normHist->GetBinContent(iCol, iRow);
+                normHist->SetBinContent(iCol, iRow, binVal/norm);
             }
         }
     }
     return normHist;
+}
+
+
+void TestNormalize(TString axis)
+{
+    TFile inFile("~/Analysis/lambda/AliAnalysisLambda/Results/2016-04/29-MC-ResMatrix/MomentumResolutionMatrices.root");
+    TDirectory *dir = inFile.GetDirectory("Rebinned");
+
+    TH2F *hist = (TH2F*) dir->Get("fResMatrixLAMixedAllRebin");
+
+    TH2* normHist = NormalizeTH2(hist, axis);
+
+    TCanvas *c1 = new TCanvas("c1", "NormMatrix");
+
+    normHist->SetDirectory(0);
+    normHist->Draw("colz");
+}
+
+void NormalizeMatricesInDir(TDirectory *inDir, TDirectory *outDir, TString axis)
+{
+    // Iterate over all the objects in the input. For each TH2 object,
+    // normalize it and save it in the output directory
+
+    // TKey *key;
+    TIter histIter(inDir->GetListOfKeys());
+    TObject *obj = NULL;
+    while ((obj = histIter())) {
+        cout << "Trying to find TH2" << endl;
+        TKey *histKey = dynamic_cast<TKey*>(obj);
+        TH2 *hist = dynamic_cast<TH2*>(histKey->ReadObj());
+        if (!hist) {
+            cout << "Object could not be cast into a TH2" << endl;
+            continue;
+        }
+
+        TH2 *normHist = NormalizeTH2(hist, axis);
+        normHist->DrawCopy();
+        outDir->cd();
+        normHist->Write(normHist->GetName(), TObject::kOverwrite);
+        cout << "Wrote " << normHist->GetName() << " to " << outDir->GetName()
+             << endl;
+    }
+}
+
+void DoResMatrixNormalizations()
+{
+    DoMatrixNormalizations("MomentumResolutionMatrices.root", "Rebinned",
+                            "Normalized", "X");
+}
+
+void DoTransformMatrixNormalizations()
+{
+    TFile matrixFile("PreparedTransformMatrices.root");
+    DoMatrixNormalizations("PreparedTransformMatrices.root", "",
+                            "Normalized", "Y");
+
+
+}
+
+void DoMatrixNormalizations(TString fileName, TString inputDirName,
+                            TString outputDirName, TString axisName)
+{
+    TFile matrixFile(fileName,"update");
+
+    TDirectory *inDir = matrixFile.GetDirectory(inputDirName);
+    if (!inDir) {
+        cout << "Could not find directory of files to rebin" << endl;
+        return;
+    }
+
+    TDirectory *outDir = matrixFile.GetDirectory(outputDirName);
+    if (!outDir) {
+        outDir = matrixFile.mkdir(outputDirName);
+    }
+
+    NormalizeMatricesInDir(inDir, outDir, axisName);
+
 }
